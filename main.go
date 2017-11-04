09cd124bac223cc967e2a8d72640cef46560c9eb
Nathan Fielding <nathan.fielding@gmail.com>
2017-11-03 20:30:49 -0400

    web spider

--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 )
 
 // https://tour.golang.org
 func main() {
+	filename := flag.String("file", "web.html", "The file to writ to")
+
+	flag.Parse()
+
+	resp, err := http.Get("http://example.com/")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	// read from exmaple.com, write to command line
+	f, _ := os.Create(*filename)
+	io.Copy(f, resp.Body)
+	fmt.Printf("saved file %s\n", *filename)
+}
+
+func printStuff() {
 	f, err := os.Create("hello.txt")
 	if err != nil {
 		panic(err)
