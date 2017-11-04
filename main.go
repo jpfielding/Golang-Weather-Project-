324c632231c2d6b5a709722fdb192a82b76fb0f8
Nathan Fielding <nathan.fielding@gmail.com>
2017-11-03 20:12:13 -0400

    checkpoint

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
+
 import (
-		"fmt" 
+	"fmt"
+	"os"
 )
+
+// https://tour.golang.org
 func main() {
-		fmt.Println("Hello, playground")
+	f, err := os.Create("hello.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
 
+	for i := 0; i < 10; i++ {
+		// to the file
+		fmt.Fprintf(f, "Hello, playground %d\n", i)
+		// writing to the terminal
+		fmt.Fprintf(os.Stdout, "Hello, playground %d\n", i)
+		fmt.Fprintf(os.Stderr, "Hello, playground %d\n", i)
+	}
 }
-
